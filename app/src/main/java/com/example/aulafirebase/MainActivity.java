5ca0b4bde6c67c4151985bb4e5b4164765295a81
joaocelso172 <50550306+joaocelso172@users.noreply.github.com>
2020-02-04 17:24:11 -0300

    Adição de spinner

--- app/src/main/java/com/example/aulafirebase/MainActivity.java
+++ app/src/main/java/com/example/aulafirebase/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.aulafirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnEnviarTarefa;
     private EditText edNome, edDesc, edPrioridade, edAssociado;
+
+    private Spinner spinNomes;
+
+    String[] nomeUsuarios = new String[]{"João", "Teste 01", "Teste 02"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +43,40 @@ public class MainActivity extends AppCompatActivity {
         edDesc = findViewById(R.id.editDescTarefa);
         edPrioridade = findViewById(R.id.editPrioridadeTarefa);
         edAssociado = findViewById(R.id.editAssociado);
+        spinNomes = findViewById(R.id.spinnerNomes);
+
+        tipoUsuario = "administrador";
+
+
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, nomeUsuarios);
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinNomes.setAdapter(arrayAdapter);
 
       /*  refenciaDb.child( "usuarios" ).child("administrador").child("nome").setValue("João");
         refenciaDb.child( "usuarios" ).child("visitantes").child("nome").setValue("Visitante Teste");*/
 
-        DatabaseReference usuariosAdd = refenciaDb.child( "usuarios" );
+       /* DatabaseReference usuariosAdd = refenciaDb.child( "usuarios" );
 
-        Usuario usuarioObj = new Usuario();
+        usuariosAdd.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i("FIREBASE ", dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+       });*/
+
+       /* Usuario usuarioObj = new Usuario(edNome.getText().toString(), "administrador");
         usuarioObj.setNome("Visitante Teste Objeto");
 
 
-        tipoUsuario = "administrador";
+       usuariosAdd.child("visitantes").setValue(usuarioObj);*/
 
-        usuariosAdd.child("visitantes").setValue(usuarioObj);
+
 
         btnEnviarTarefa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,22 +93,28 @@ public class MainActivity extends AppCompatActivity {
 
         Tarefa tarefaObj = new Tarefa();
 
-        if (!edNome.getText().toString().equals(null) || !edDesc.getText().toString().equals(null) || !edAssociado.getText().toString().equals(null)
-                || !edPrioridade.getText().toString().equals(null)) {
+        if (!spinNomes.getSelectedItem().toString().equals("") || !edDesc.getText().toString().equals("") || !edAssociado.getText().toString().equals("")
+                || !edPrioridade.getText().toString().equals("")) {
 
-            tarefaObj.setNomeTarefa(edNome.getText().toString());
-            tarefaObj.setPrioridadeTarefa(Integer.parseInt(edPrioridade.getText().toString()));
-            tarefaObj.setDescTarefa(edDesc.getText().toString());
-            tarefaObj.setPessoaAtribuida(edAssociado.getText().toString());
+            try {
+                tarefaObj.setNomeTarefa(spinNomes.getSelectedItem().toString());
+                tarefaObj.setPrioridadeTarefa(Integer.parseInt(edPrioridade.getText().toString()));
+                tarefaObj.setDescTarefa(edDesc.getText().toString());
+                tarefaObj.setPessoaAtribuida(edAssociado.getText().toString());
 
-            DatabaseReference tarefasAdd = refenciaDb.child("usuarios").child(tipoUsuario).child(tarefaObj.getPessoaAtribuida()).child(tarefaObj.getNomeTarefa());
-            tarefasAdd.child("desc da Tarefa").setValue(tarefaObj.getDescTarefa());
-            tarefasAdd.child("pessoa Atribuida").setValue(tarefaObj.getPessoaAtribuida());
-            tarefasAdd.child("prioridade da tarefa").setValue(tarefaObj.getPrioridadeTarefa());
+                DatabaseReference tarefasAdd = refenciaDb.child("usuarios").child(tipoUsuario).child(tarefaObj.getPessoaAtribuida()).child(tarefaObj.getNomeTarefa());
+                tarefasAdd.child("desc da Tarefa").setValue(tarefaObj.getDescTarefa());
+                tarefasAdd.child("pessoa Atribuida").setValue(tarefaObj.getPessoaAtribuida());
+                tarefasAdd.child("prioridade da tarefa").setValue(tarefaObj.getPrioridadeTarefa());
+
+                Toast.makeText(this, "Dados gravados com sucesso!", Toast.LENGTH_LONG).show();
+            }catch (Exception e){
+                Toast.makeText(this, "Houve um erro ao tentar gravar: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         }
 
         else {
-            Toast.makeText(this, "Preencha todos os campos antes de continuar...\n\t :)", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Preencha todos os campos antes de continuar.  ;)", Toast.LENGTH_LONG).show();
         }
 
     }
--- app/src/main/java/com/example/aulafirebase/Usuario.java
+++ app/src/main/java/com/example/aulafirebase/Usuario.java
@@ -3,13 +3,10 @@ package com.example.aulafirebase;
 public class Usuario {
 
     private String nome;
+    private String nivelUsuario;
 
     public Usuario(){
 
-    }
-
-    public Usuario(String nome) {
-        this.nome = nome;
     }
 
     public String getNome() {
@@ -18,5 +15,18 @@ public class Usuario {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getNivelUsuario() {
+        return nivelUsuario;
+    }
+
+    public void setNivelUsuario(String nivelUsuario) {
+        this.nivelUsuario = nivelUsuario;
+    }
+
+    public Usuario(String nome, String nivelUsuario) {
+        this.nome = nome;
+        this.nivelUsuario = nivelUsuario;
     }
 }
